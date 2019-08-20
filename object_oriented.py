d9e9bef058120419f999154cdc8da24d807b7873
Ana Caroline Ferreira <ana.ferreira@pti.org.br>
2019-08-20 16:15:27 -0300

    orientacao a objetos

--- object_oriented.py
+++ object_oriented.py
@@ -4,8 +4,15 @@ Orientação a objetos
 '''
 class Dog:
     kind = 'canine'
+    tricks = []
     def __init__(self, name):
         self.name = name
+        self.tricks = []
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
 
 our_dog = Dog('Sissi')
-print(our_dog.name)
+print(our_dog.name, our_dog.kind)
+our_dog.add_trick('roll over')
+print(our_dog.tricks)
