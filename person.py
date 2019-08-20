d0fb38b8404168991ff54e9e0175627421ad9bf0
Ana Caroline Ferreira <ana.ferreira@pti.org.br>
2019-08-20 16:31:46 -0300

    orientacao a objetos

--- person.py
+++ person.py
@@ -24,3 +24,20 @@ ana = Person('ana', 'ferreira')
 print(ana.get_name())
 ana_employee = Employee('ana', 'ferreira', 1)
 print(ana_employee.get_employee())
+
+
+class Example:
+    def __init__(self):
+        print("This is a static method example class")
+
+    @classmethod
+    def class_example_function(cls):
+        print("I am a class method")
+        cls.static_example_function()
+
+    @staticmethod
+    def static_example_function():
+        print("I am a static method")
+
+ex = Example()
+ex.class_example_function()
